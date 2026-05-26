7125d2659c9ab6dbdf7e82294d1d7aa1048a511a
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2026-05-26 17:20:39 -0400

    hopefully fix vpn drops
    issue where trying to reattach to a session after a vpn is dropped is broken

--- smux/__init__.py
+++ smux/__init__.py
@@ -193,7 +193,7 @@ while [ -e /proc/$pid ]; do sleep 5; done
         time.sleep(1)
         import shutil
         srunpath = shutil.which('srun')
-        os.execv(srunpath,["srun","--pty","-O","--ntasks","1","--jobid","{}".format(jobid),"tmux","attach-session","-t","{}".format(name)])
+        os.execv(srunpath,["srun","--overlap","--pty","-O","--ntasks","1","--jobid","{}".format(jobid),"tmux","attach-session","-t","{}".format(name)])
 
     @classmethod
     def connectJob(cls,args):
